af0816904355f7a44990409b4a23ccb5e0643e84
samwanyua <samexample8@gmail.com>
2024-03-06 00:20:05 +0300

    Implemented string concatenation

--- string_formatting.py
+++ string_formatting.py
@@ -3,3 +3,20 @@
 person = {'name': 'Jenn', 'age': 23}
 sentence = f"My name is {person['name']} and I am {person['age']} years old."
 print(sentence)
+
+calculation = f" 4 times 11 is equal to {4 * 11}"
+print(calculation)
+
+for n in range(1,11):
+    sent = f"The value is {n:07}" # to add zero padding
+    print(sent)
+
+pi = 3.14159265
+line = f"Pi is equal to {pi:.2f}"  #to 4 digits
+print(line)
+
+# printing dates
+from datetime import datetime
+birthday = datetime(1990,4,1)
+line_b = f"Jenn has a birthday on {birthday:%B %d, %Y}" # Check python documentation
+print(line_b)
